d9369a75968049dd644171f77d92bf481289493d
Nathan <skeks463@gmail.com>
2020-02-13 11:07:26 +0900

    Ch05 code & application install error

---
+++ src/main/java/me/nathan/book/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,19 @@
+package me.nathan.book.springboot.config.auth.dto;
+
+import lombok.Getter;
+import me.nathan.book.springboot.domain.user.User;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getEmail();
+    }
+}
--- src/main/java/me/nathan/book/springboot/web/IndexController.java
+++ src/main/java/me/nathan/book/springboot/web/IndexController.java
@@ -1,6 +1,8 @@
 package me.nathan.book.springboot.web;
 
 import lombok.RequiredArgsConstructor;
+import me.nathan.book.springboot.config.auth.dto.SessionUser;
+import me.nathan.book.springboot.domain.user.User;
 import me.nathan.book.springboot.service.PostsService;
 import me.nathan.book.springboot.web.dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
@@ -8,16 +10,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("posts", postsService
-                            .findAllDesc());
+        model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
@@ -31,6 +41,6 @@ public class IndexController {
         PostsResponseDto dto = postsService.findById(id);
         model.addAttribute("post", dto);
 
-        return "post-update";
+        return "posts-update";
     }
 }
